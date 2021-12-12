68b7ee6e97034335dcbef22bdb2a0bcbaa8be276
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-12-12 07:53:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+float numM (const float num[], int dim){
+    num[dim];
+    float max = num[0];
+    for (int i = 0; i < dim; ++i) {
+        if(max<num[i]){
+            max=num[i];
+        }
 
+    }
+    return max;
+}
