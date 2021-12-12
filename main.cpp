65f5a23eb6798ef6d370ed1279b895751ae255c9
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-12-12 07:55:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+
+int main() {
+    int dim;
+    cin>>dim;
+    float num[dim];
+    for (int i = 0; i < dim; ++i) {
+        cin>>num[i];
+
+    }
+    numM(num,dim);
+    cout<<numM(num, dim)<<endl;
+    return 0;
 }
